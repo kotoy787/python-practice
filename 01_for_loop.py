74d8d50ef036d7a6bcf9b8b35b3296654b62384b
Артур <kotoy1337@gmail.com>
2026-09-13 14:03:59 +0300

    for i in range(start, stop, step)

--- 01_for_loop.py
+++ 01_for_loop.py
@@ -36,4 +36,36 @@ for i in range(100, 1000):  # перебираем числа от 100 до 999
     if i % 10 == 7:         # используем остаток от деления на 10, для получения последней цифры
         print(i)      
 
-    
+# Последовать от 100 до 999
+for i in range(100, 1000):
+    if i % 10 == 7:
+        print(i)
+
+# Задача отобразить диапозон от m до n
+m = int(input())
+n = int(input())
+for i in range(m, n+1):
+    print(m, i+1)
+
+# Таблица умножения от *1 до * 10  
+num = int(input())
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i) 
+     
+# Числа в диапозоне от m до n, где (m > n) и только нечетные
+m = int(input())
+n = int(input())
+for i in range(m , n -1, -1):
+    if i % 2 != 0:
+        print(i)            
+
+# 2 в 1
+
+m = int(input())
+n = int(input())
+if m <= n:
+    for i in range(m, n + 1):
+        print(i)
+elif m > n:
+    for i in range(m, n - 1, -1):
+        print(i)    
